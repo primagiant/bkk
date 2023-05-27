665d6366aa14b02c7b47c4402c22c4b95b2317ff
Prima Giant <kadekprimagiantmartadinata@gmail.com>
2023-05-27 14:15:56 +0800

    adding dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,16 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('BKK') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+    <h1 class="text-4xl mb-8 text-center font-black">
+        Sistem Kendali Program Aktifitas - Badan Kerjasama dan Kehumasan
+    </h1>
+    <hr>
+    <div class="py-8">
+        <h2 class="text-xl font-bold">Rekap Program</h2>
+        <div class="mt-6 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-4">
+            @for ($i = 0; $i < 10; $i++) <div
+                class="flex flex-col justify-center items-center bg-gray-200 px-4 pt-8 pb-4 rounded-lg">
+                <p class="text-xl font-bold">50</p>
+                <p class="text-sm mt-3 font-light">Tahun 2021</p>
+        </div>@endfor
+    </div>
     </div>
 </x-app-layout>
